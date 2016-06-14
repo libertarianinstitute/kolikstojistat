54239df4d6e0adb428c7ab681dbe4a4beb574bf1
Michal Kurz <michal.kurz@libertariani.cz>
2016-06-14 16:02:21 +0200

    Actual page alpha

---
+++ _site/js/ui.js
@@ -0,0 +1,6 @@
+var topmenu = document.getElementById('home').onclick=function(){ 
+	window.location.replace("http://stackoverflow.com")
+};
+
+
+// window.location.replace("http://stackoverflow.com");
